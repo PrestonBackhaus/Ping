36ae1c8d26a98a22a311491549e25d00f885ba9a
Preston <54640348+PrestonBackhaus@users.noreply.github.com>
2023-04-04 16:54:22 -0400

    A

--- CheckURL/single.py
+++ CheckURL/single.py
@@ -5,11 +5,13 @@ class SingleChecker:
     def __init__(self, source_url):
         self.output = ''
         self.source_url = source_url
-        self.found_urls = []
+        self.urls = []
+        self.trueURLs = []
         
     def polish(self):
         polish_chars = ['ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż']
         english_chars = ['a', 'c', 'e', 'l', 'n', 'o', 's', 'z', 'z']
+        top = ['.com', '.org', '.net', '.gov', '.edu', '.mil', '.int', '.info', '.biz', '.name', '.pro', '.coop', '.mobi', '.travel', '.jobs', '.tel']
         http = self.source_url.split(':')[0]
         domain = urlparse(self.source_url).netloc
         domain = domain.replace('www.','')
@@ -17,27 +19,34 @@ class SingleChecker:
         domain = domain.split('.')[0]
         for i in range(len(polish_chars)):
             domain = domain.replace(english_chars[i], polish_chars[i])
-        self.source_url = f'{http}://www.{domain}.{after_domain}'
+        self.urls = [f'{http}://www.{domain}{t}' for t in top]
         
-    def checkURL(self):
-        try:
-            response = requests.get(self.source_url)
-            if response.status_code == 200:
-                print(f'{self.source_url} is already online.\n\n')
-                self.output = f'{self.source_url} is already online.\n\n'
-            else:
-                print(f'{self.source_url} is offline.\n\n')
-                self.output = f'{self.source_url} is offline.\n\n'
-        except requests.exceptions.RequestException as e:
-            print(f'{self.source_url} is offline or not in use: {e}')
+    def checkURL(self, url):
+        if url=='':
             print('URL INVALID\n\n')
-            self.output = f'{self.source_url} is offline or not in use: {e}\nURL INVALID'
+            self.output = 'URL INVALID\n\n'
+        else:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    print(f'{url} is already online.\n\n')
+                    self.output = f'{url} is already online.\n\n'
+                    self.trueURLs.append(url)
+                else:
+                    print(f'{url} is offline.\n\n')
+                    self.output = f'{url} is offline.\n\n'
+            except requests.exceptions.RequestException as e:
+                print(f'{url} is offline or not in use: {e}')
+                print('URL INVALID\n\n')
+                self.output = f'{url} is offline or not in use: {e}\nURL INVALID'
             
     def getOutput(self):
         return self.output
+
             
     def run(self):
         self.polish()
-        self.checkURL()
-        return self.output
-        
+        self.checkURL(self.source_url)
+        for url in self.urls:
+            self.checkURL(url)
+        return self.trueURLs
